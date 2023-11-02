8307a4048f63fdacd9aa2d3eacc49082afffb953
Bolkof <142789552+Bolkof@users.noreply.github.com>
2023-11-02 10:58:32 -0500

    Create paso1.py

---
+++ paso1.py
@@ -0,0 +1,18 @@
+import markdown
+
+# Nombre del archivo de entrada (Markdown) y de salida (HTML)
+archivo_entrada = 'mi_archivo.md'
+archivo_salida = 'mi_archivo.html'
+
+# Abre el archivo de entrada y lee el contenido
+with open(archivo_entrada, 'r', encoding='utf-8') as f:
+    contenido_markdown = f.read()
+
+# Convierte el contenido Markdown a HTML
+contenido_html = markdown.markdown(contenido_markdown)
+
+# Escribe el contenido HTML en el archivo de salida
+with open(archivo_salida, 'w', encoding='utf-8') as f:
+    f.write(contenido_html)
+
+print(f'Se ha convertido "{archivo_entrada}" a "{archivo_salida}" con éxito.')
