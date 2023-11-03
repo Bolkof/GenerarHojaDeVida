ede282b0e0610cc244abe5050e4aa7ec4c5e4a75
Bolkof <142789552+Bolkof@users.noreply.github.com>
2023-11-02 22:56:16 -0500

    Actualizar final.py

--- final.py
+++ final.py
@@ -14,18 +14,19 @@ contenido_html = markdown.markdown(contenido_markdown)
 # Escribe el contenido HTML en el archivo de salida
 with open(archivo_salida, 'w', encoding='utf-8') as f:
     f.write(contenido_html)
-    
 
-
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 
 # Nombre del archivo de entrada (HTML) y de salida (PDF)
 archivo_html = 'muestra.html'
 archivo_css = 'estilo.css'
 archivo_pdf = 'final.pdf'
 
+# Define un estilo CSS para eliminar márgenes
+css = CSS(string='@page { margin: 0; }')
+
 # Crea un objeto HTML a partir del archivo HTML y aplica el CSS
 html = HTML(filename=archivo_html)
-html.write_pdf(archivo_pdf, stylesheets=[archivo_css])
+html.write_pdf(archivo_pdf, stylesheets=[css])
 
 print(f'Se ha convertido "{archivo_entrada}" a "{archivo_pdf}" con éxito.')
