3bab349bf8823863a5aad06d4524fb608ed58b98
Bolkof <142789552+Bolkof@users.noreply.github.com>
2023-11-03 05:27:13 -0500

    Actualizar previsualizacion.py

--- previsualizacion.py
+++ previsualizacion.py
@@ -11,8 +11,30 @@ with open(archivo_entrada, 'r', encoding='utf-8') as f:
 # Convierte el contenido Markdown a HTML
 contenido_html = markdown.markdown(contenido_markdown)
 
-# Escribe el contenido HTML en el archivo de salida
+# Código HTML al principio
+html_inicio = """
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="estilo.css">
+  <title>Mi Hoja de Vida</title>
+</head>
+<body>
+"""
+
+# Código HTML al final
+html_final = """
+</body>
+</html>
+"""
+
+# Combinar el contenido HTML generado con el código HTML al principio y al final
+contenido_html_completo = html_inicio + contenido_html + html_final
+
+# Escribe el contenido HTML completo en el archivo de salida
 with open(archivo_salida, 'w', encoding='utf-8') as f:
-    f.write(contenido_html)
+    f.write(contenido_html_completo)
 
 print(f'Se ha convertido "{archivo_entrada}" a "{archivo_salida}" con éxito.')
